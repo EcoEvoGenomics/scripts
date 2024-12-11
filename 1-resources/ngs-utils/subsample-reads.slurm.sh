8b96113df7dba91c311b6d4c37a57bd9f6248a5b
Erik Sandertun Røed <e.s.roed@ibv.uio.no>
2024-12-11 14:16:06 +0100

    Add 1-resources/ngs-utils/subsample-reads.slurm.sh

---
+++ 1-resources/ngs-utils/subsample-reads.slurm.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# ADMIN
+#SBATCH --job-name=subsample_reads
+#SBATCH --output=SLURM-%j-%x.out
+#SBATCH --error=SLURM-%j-%x.err
+#SBATCH --account=nn10082k
+#
+# RESOURCE ALLOCATION
+#SBATCH --nodes=1
+#SBATCH --tasks=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=4G
+#SBATCH --time=02:00:00
+
+# User definitions
+reads_directory=
+sampled_reads=
+
+# Keep low for large FASTA files to reduce memory use!
+proportion_of_reads_sampled_from=0.1
+
+# Prepare environment
+set -o errexit
+set -o nounset
+module --quiet purge
+
+# Load modules
+module load SeqKit/2.3.1
+
+# Begin work
+cd $reads_directory
+mkdir sample
+
+echo "Writing list of read files in directory ${reads_directory} ..."
+find "$PWD"/ -type f | grep *.fastq.gz | sort > sample/reads_sample.list
+
+echo "Subsampling reads ..."
+while read read_file; do
+    read_basename=$(basename $read_file ".fastq.gz")
+    echo "Subsampling: ${read_basename}.fastq.gz ..."
+    seqkit sample -p ${proportion_of_reads_sampled_from} ${read_basename}.fastq.gz |\
+    seqkit head -n ${sampled_reads} -o sample/${read_basename}_sample.fastq.gz
+done <sample/reads_sample.list
+
+echo "Done subsampling!"
+# Work end
