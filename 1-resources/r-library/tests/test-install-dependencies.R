64d8b375aabcd20410d03f2af005002a3027b043
Erik Sandertun Røed <e.s.roed@ibv.uio.no>
2024-12-19 15:43:17 +0100

    Add testing scripts

---
+++ 1-resources/r-library/tests/test-install-dependencies.R
@@ -0,0 +1,12 @@
+#-----------------------------------------------------#
+# Packages required to run tests with GitHub Actions. #
+# Please keep up-to-date when expanding the library.  #
+#-----------------------------------------------------#
+install.packages(
+  c(
+    "testthat",
+    "R6",
+    "stringr",
+    "dplyr"
+  )
+)
