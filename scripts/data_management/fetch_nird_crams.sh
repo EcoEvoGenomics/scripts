e2b22fb2678f30f12566b3cafd6fb0b86369d47d
Erik Sandertun Røed <e.s.roed@ibv.uio.no>
2025-08-05 01:51:49 +0200

    Update in-file documentation of fetch_nird_*.sh

--- scripts/data_management/fetch_nird_crams.sh
+++ scripts/data_management/fetch_nird_crams.sh
@@ -2,9 +2,9 @@
 
 # NB!
 #
-#     1. This script requires an internet connection.
-#     On e.g. Saga, this is limited on compute nodes. Run
-#     this from a login node.
+#     1. This script requires that NIRD is mounted.
+#     On Saga, this is not the case on compute nodes.
+#     Run this from a login node.
 #
 #     2. This script can take a long time to complete.
 #     To ensure it runs to completion, run it on a
